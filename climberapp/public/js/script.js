e1bb787d9f129aa1e35eb20a9c67753e6f7299f5
Michael Werbowsky <mwerbo2@gmail.com>
2016-07-02 11:45:20 -0400

    setting up search object and ajax call

--- climberapp/public/js/script.js
+++ climberapp/public/js/script.js
@@ -1,7 +1,43 @@
 $(document).ready(function() {
 
 
+const $body = $('body')
+const $climbs =  $('<climbs>')
+const $results = $('#results')
 
+$climbs.addClass('climbs')
+
+$('button').click(function(){
+  console.log('clicked')
+  // $results.empty()
+  const queryObject = {}
+  console.log(queryObject)
+
+  if ($('.location').val() !== '') queryObject.location = $('.location').val();
+  console.log(queryObject)
+  if ($('.rating').val() !== '') queryObject.rating = $('.rating').val();
+  console.log(queryObject)
+  if ($('.type').val() !== '') queryObject.type = $('.type').val();
+  console.log(queryObject)
+
+
+$.ajax({
+  url: '/climbs',
+  type: 'GET',
+  dataType: 'json',
+  data: queryObject,
+  success: function(data) {
+    console.log(data)
+  }
+})
+
+
+
+
+
+
+
+})
 
 
 
@@ -18,9 +54,3 @@ $(document).ready(function() {
 
 //on click trigger -get the value the run the ajax
 
-// .$.ajax({
-//   url: '/path/to/file',
-//   type: 'default GET (Other values: POST)',
-//   dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-//   data: {param1: 'value1'},
-// })a
