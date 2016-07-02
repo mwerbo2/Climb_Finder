b2857227dae9c515e945a524fd7ac8bd2a4facfc
Michael Werbowsky <mwerbo2@gmail.com>
2016-07-02 18:45:16 -0400

    working on the user login

--- climberapp/app.js
+++ climberapp/app.js
@@ -3,6 +3,8 @@ const express           = require('express');
 const path              = require('path');
 const logger            = require('morgan');
 const bodyParser        = require('body-parser');
+const session           = require('express-session');
+const methodOverride    = require('method-override');
 const app               = express();
 const port              = process.env.PORT || 3000
 // const routeData = require('routesData');
@@ -13,6 +15,17 @@ const climbRoute        = require('./routes/climb');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+app.use(session({
+  saveUnitialized:true,
+  resave: true,
+  secret: 'secrectsecret',
+  cookie: {maxAge: 60000}
+}));
+
+
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
