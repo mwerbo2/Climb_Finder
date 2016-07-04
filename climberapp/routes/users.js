cce16fe3b2a57fd39692ff0c0c4bfd4e51d39eed
Michael Werbowsky <mwerbo2@gmail.com>
2016-07-04 15:10:16 -0400

    set up user authentication

--- climberapp/routes/users.js
+++ climberapp/routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const routesData = require('../models/climbs');
+const { createUser, loginUser } = require('../models/users');
 
 // router.get('/find', function(req, res){
 //   res.json(routesDataTwo)
@@ -11,11 +11,32 @@ router.get('/register', function(req, res){
   res.render('register')
 })
 
+router.post('/register', createUser, function(req, res){
+  console.log(req.body);
+  res.redirect('/')
+})
+
 
 router.get('/login', function(req, res){
   console.log("got to the login route")
   res.render('login')
 })
+
+router.post('/login', loginUser, function(req, res){
+  console.log(res.user);
+  req.session.user = res.user;
+
+  req.session.save(function(err){
+    if(err) throw err;
+    res.redirect('/');
+  });
+});
+
+router.delete('/logout', function(req, res){
+  req,session.destroy(function(err){
+    res.redirect('/')
+  });
+});
 
 
 module.exports = router;
