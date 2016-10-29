9c20c252afbe875dbe5d9fef79765be5575fb449
Michael Werbowsky <mwerbo2@gmail.com>
2016-10-29 19:14:10 -0400

    adding plan for calendar

--- models/scheduling.js
+++ models/scheduling.js
@@ -3,7 +3,7 @@ const MongoClient       = require('mongodb').MongoClient;
 const dbConnection      = process.env['MONGODB_URI'] ||'mongodb://localhost:27017/climber'
 
 
-
+//build out calendar save methods
 module.exports{
 
 
