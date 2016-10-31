24f873c40968b97b82c0c6b466b844f5dea914a5
Michael Werbowsky <mwerbo2@gmail.com>
2016-10-30 23:13:15 -0400

    planning

--- models/scheduling.js
+++ models/scheduling.js
@@ -1,4 +1,4 @@
-const MongoClient       = require('mongodb').MongoClient;
+const MongoClient       = require('mongodb').MongoClient;  this
 // const dbConnection = ('mongodb://localhost:27017/climber')
 const dbConnection      = process.env['MONGODB_URI'] ||'mongodb://localhost:27017/climber'
 
